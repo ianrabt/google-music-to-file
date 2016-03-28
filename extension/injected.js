09a270a6aa17b17c5107fb6c42cd1e1bdc5e3434
Ian Taylor <ianrabt@gmail.com>
2016-03-27 20:56:46 -0700

    fix problems observing song changes if the tab loads in the background

--- extension/injected.js
+++ extension/injected.js
@@ -1,16 +1,57 @@
-function songChanged(mutations, observer) {
-		var title = document.getElementById("currently-playing-title").innerHTML;
-		var artist = document.getElementById("player-artist").innerHTML;
-		chrome.runtime.sendMessage({currentTitle: title, currentArtist: artist});
+var visibilityObserverCreated = false;
+
+function sendMessage (playing, title, artist, album) {
+		chrome.runtime.sendMessage({
+				state: playing ? "playing" : "stopped",
+				currentTitle: title,
+				currentArtist: artist
+		});
 }
 
-window.onload = function() {
-		alert('new music');
-		var songInfoWrapper = document.getElementById("playerSongInfo");
+function songChanged (mutations, observer) {
+		var title = document.getElementById("currently-playing-title").innerHTML;
+		var artist = document.getElementById("player-artist").innerHTML;
+		sendMessage(true, title, artist);
+}
+
+// returns null if the page was opened without focus (and therefore is not fully loaded)
+function getInfoWrapper () {
+		var wrapper = document.getElementById("playerSongInfo");
+		return wrapper;
+} 
+
+// watch the wrapper element, and send updated song information to background.js
+function startObservers () {
+		// now that the tab has focus, remove the observer for visibility changes
+		if (visibilityObserverCreated) {
+				document.removeEventListener("visibilitychange", startObservers);
+				visibilityObserverCreated = false;
+				console.log("loading extension");
+		}
+
+		songInfoElement = getInfoWrapper();
+
+		// loop this function until songInfoElement can be set (i.e. the page is fully loaded)
+		if (songInfoElement == null) {
+				window.setTimeout(startObservers, 2500);
+				return;
+		}
 
 		var infoObserver = new MutationObserver(songChanged);
-		infoObserver.observe(songInfoWrapper, {attributes: false, childList: true, characterData: false});
+		infoObserver.observe(songInfoElement, {attributes: false, childList: true, characterData: false});
+}
+
+window.onload = function () {
+		sendMessage(false);
+
+		if (document.hidden) {
+				document.addEventListener("visibilitychange", startObservers);
+				visibilityObserverCreated = true;
+				return;
+		}
+		startObservers();
 };
 
-window.onunload = function() {
+window.onunload = function () {
+		sendMessage(false);
 }
