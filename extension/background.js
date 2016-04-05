8664465ebe57c2e5371db596fc08694023d53cae
Ian Taylor <ianrabt@gmail.com>
2016-04-04 17:38:52 -0700

    add browser action to open options page

--- extension/background.js
+++ extension/background.js
@@ -1,18 +1,28 @@
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
+		function(request, sender, sendResponse) {
 			console.log(request.currentTitle);
 			console.log(request.currentArtist);
 
 			chrome.runtime.sendNativeMessage(
-				'net.ianrabt.google_music_to_file',
-				{
+					'net.ianrabt.google_music_to_file',
+					{
 						text: request.currentTitle + " - " + request.currentArtist,
 						path: "/home/ianrtaylor/test"
-				},
+					},
 
-				function(response) {
+					function(response) {
 						console.log("received " + response);
-				}
+					}
 			);
-	}
+		}
+);
+
+chrome.browserAction.onClicked.addListener(
+		function() {
+			if(chrome.runtime.openOptionsPage) {
+				chrome.runtime.openOptionsPage();
+			} else {
+				window.open(chrome.runtime.getURL('options/options.html'));
+			}
+		}
 );
